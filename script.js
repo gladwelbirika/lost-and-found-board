7708d434ce0b0abdff7b5f8ebae9380546dec662
Gladwel Birika <gladwel.birika@student.moringaschool.com>
2026-04-18 19:27:40 +0300

    retrieve stored items from localStorage and initoalize empty array fallback

--- script.js
+++ script.js
@@ -31,4 +31,5 @@ form.reset();
 //display reported items to the board page
 function displayItems(){
    const container = document.querySelector(".board-container"); 
+   let items = JSON.parse(localStorage.getItem("items")) || [];
 }
